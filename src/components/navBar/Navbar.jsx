c1fcac120baef02d034a5d2735d651d92cac77e5
Andrea Menendez <andreaveronicam96@gmail.com>
2023-09-15 17:53:54 -0300

    navbar created

---
+++ src/components/navBar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../../assets/codigo-de-la-computadora-portatil.png'
+import contacto from '../../assets/gmail.png'
+import {Link} from 'react-scroll'
+import styles from './navBar.module.css'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className={styles.navbar}>
+            <img src={logo} alt="logo" className={styles.logo}/>
+            <div className="desktopMenu">
+                 <Link>
+                    <Link className={styles.menuListItem}>Home</Link>
+                    <Link className={styles.menuListItem}>Sobre mi</Link>
+                    <Link className={styles.menuListItem}>Portfolio</Link>
+                    <Link className={styles.menuListItem}>Contacto</Link>
+                 </Link>
+            </div>
+            <div className={styles.menuBtn}>
+                <img src={contacto} alt="logo de gmail" className={styles.gmail} /> Contactame
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
